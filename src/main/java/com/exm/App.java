8e4f4ffe1650922d823e42d99662f5cae92ae7fd
yyy <tt@t.com>
2018-04-28 09:19:33 +0800

    init

---
+++ src/main/java/com/exm/App.java
@@ -0,0 +1,16 @@
+package com.exm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+
+    public void testMethod(){
+        System.out.println("This is testMethod !");
+    }
+}
